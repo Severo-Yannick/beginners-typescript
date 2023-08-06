3771c184b4358145c8598e1717ef44f891bb6207
Severo-Yannick <severoyannick@gmail.com>
2023-08-06 11:24:19 +0200

    number problem

---
+++ src/number/number.problem.ts
@@ -0,0 +1,5 @@
+// Parameter 'numberOne' implicitly has an 'any' type, but a better type may be inferred from usage.ts(7044)
+
+export const addTwoNumber = (numberOne, numberTwo) => {
+  return numberOne + numberTwo;
+};

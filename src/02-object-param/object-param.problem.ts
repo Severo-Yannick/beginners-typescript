ee6466a757049dc6fc6982c6420c64d3356b601a
Severo-Yannick <severoyannick@gmail.com>
2023-08-06 11:45:17 +0200

    object-param problem

---
+++ src/02-object-param/object-param.problem.ts
@@ -0,0 +1,5 @@
+// Parameter 'params' implicitly has an 'any' type, but a better type may be inferred from usage.ts(7044)
+
+export const addTwoNumbers = (params) => {
+  return params.first + params.second;
+};

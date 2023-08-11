b68655fd54fe234491c1f1d5668a913a51bbbf2b
Severo-Yannick <severoyannick@gmail.com>
2023-08-11 19:13:37 +0200

    fn-return-type-annotations problem

---
+++ src/08-functions/fn-return-type-annotations.problem.ts
@@ -0,0 +1,20 @@
+interface User {
+  id: number;
+  firstName: string;
+  lastName: string;
+  role: "admin" | "user" | "super-admin";
+  posts: Array<Post>;
+}
+
+interface Post {
+  id: number;
+  title: string;
+}
+
+/**
+ * How do we ensure that makeUser ALWAYS
+ * returns a user?
+ */
+const makeUser = () => {
+  return {};
+};
